19ec082eee6ad6e18a225dbd541f8411aefe6a3f
GenghiSkeleton <24704187+GenghiSkeleton@users.noreply.github.com>
2017-09-12 00:00:22 -0300

    Update main.c
    Agregue menu_registro

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include "main.h"
 
+int menu_registro(int *ptr_padron, int *ptr_num_carrera, char carrera[][30], char apellido[], char nombre[]);
+
+void imprimir_carrera(int fila, char carrera[][30]);
 
 int main(void)
 {
+	/*Cambiar constantes a constantes en serio.*/
 	estado_main estado = MAIN_MENU;
 	int input_i = 0;
+	char apellido[50];
+	char nombre[50];
+	int padron, num_carrera;
+	char carrera[11][30] = {{ING_1}, {ING_2}, {ING_3}, {ING_4}, {ING_5}, {ING_6}, {ING_7}, {ING_8}, {ING_9}, {ING_10}, {ING_11}};
+
 
 	puts(MSJ_BIENVENIDA);
 
@@ -39,23 +48,22 @@ int main(void)
 					fprintf(stderr,"%s: %s\n", ERR_PREFIJO, ERR_OPCIONES);
 					return EXIT_FAILURE;
 				}
+
 				break;
 			}
 
 			case MENU_REGISTRO:
 			{
-				/* hacer funcion type registro( variables a discutir ) */
-				puts("Menu Registro! Pulse 0 para volver y cualquier otra cosa para un error fatal!");
+				input_i = menu_registro(&padron, &num_carrera, carrera, apellido, nombre);
 
-				scanf("%i", &input_i);
-
-				if(input_i == 0)
+				if(input_i == EXIT_SUCCESS)
 					estado = MAIN_MENU;
 				else
 				{
-					puts(ERR_PREFIJO);
 					return EXIT_FAILURE;
 				}
+
+				printf("PRUEBA: PADRON: %i CARRERA: %i\n", padron, num_carrera);
 
 				break;
 			}
@@ -74,7 +82,6 @@ int main(void)
 					puts(ERR_PREFIJO);
 					return EXIT_FAILURE;
 				}
-
 				break;
 			}
 
@@ -98,7 +105,7 @@ int main(void)
 
 			case MENU_FINALIZAR:
 			{
-				puts("Menu FInalizar! Aquí se imprimiran los datos calculados y se volverá a cero los valores. Pulse 0 para volver.");
+				puts("Menu Finalizar! Aquí se imprimiran los datos calculados y se volverá a cero los valores. Pulse 0 para volver.");
 
 				scanf("%i", &input_i);
 
@@ -113,10 +120,90 @@ int main(void)
 			case MAIN_SALIR:
 			{
 				puts(MSJ_DESPEDIDA);
-				/* Funcion p' imprimir datos ingresados? */
 				return EXIT_SUCCESS;
 			}
 
 		}
 	}
+}
+
+void imprimir_carrera(int fila, char carrera[][30])
+{
+       	printf("%s\n", carrera[fila - 1]);
+}
+
+int menu_registro(int *ptr_padron, int *ptr_num_carrera, char carrera[][30], char apellido[], char nombre[])
+{
+	int input_i = 0;
+
+	puts(MSJ_REGISTRO);
+
+	while(1)
+	{
+		printf("1) %s\n2) %s\n3) %s\n0) %s\n", REGISTRO_OPCION_NOMBRE, REGISTRO_OPCION_PADRON, REGISTRO_OPCION_CARRERA, OPCION_VOLVER);
+
+		if(scanf("%i", &input_i) != 1)
+		{
+			fprintf(stderr, "%s: %s\n", ERR_PREFIJO, ERR_OPCIONES);
+			return EXIT_FAILURE;
+		}
+		while(getchar() != '\n');
+
+		if(input_i == 1)
+		{
+			printf("%s: ", REGISTRO_ING_APELLIDO);
+			if(scanf("%s", apellido) != 1)
+			{
+				fprintf(stderr, "%s: %s\n", ERR_PREFIJO, ERR_REG_NOMBRE);
+				return EXIT_FAILURE;
+			}
+			while(getchar() != '\n');
+
+			printf("%s: ", REGISTRO_ING_NOMBRE);
+
+			if(scanf("%s", nombre) != 1)
+			{
+				fprintf(stderr, "%s: %s\n", ERR_PREFIJO, ERR_REG_NOMBRE);
+				return EXIT_FAILURE;
+			}
+			while(getchar() != '\n');
+
+			printf("%s: %s, %s\n", REGISTRO_ING_AVISO, apellido, nombre);
+		}
+		else if(input_i == 2)
+		{
+			printf("%s: ", REGISTRO_ING_PADRON);
+			if(scanf("%i", ptr_padron) != 1)
+			{
+				fprintf(stderr, "%s: %s\n", ERR_PREFIJO, ERR_REG_PADRON);
+				return EXIT_FAILURE;
+			}
+			while(getchar() != '\n');
+			printf("%s: %i\n", REGISTRO_ING_AVISO, *ptr_padron);
+		}
+		else if(input_i == 3)
+		{
+			printf("%s: ", REGISTRO_ING_CARRERA);
+			if(scanf("%i", ptr_num_carrera) != 1)
+			{
+				fprintf(stderr, "%s: %s\n", ERR_PREFIJO, ERR_REG_CARRERA);
+				return EXIT_FAILURE;
+			}
+			while(getchar() != '\n');
+
+			printf("%s: ", REGISTRO_ING_AVISO);
+			imprimir_carrera(*ptr_num_carrera, carrera);
+		}
+		else if(input_i == 0)
+		{
+			break;
+		}
+		else
+		{
+			fprintf(stderr, "%s: %s\n", ERR_PREFIJO, ERR_OPCIONES);
+			return EXIT_FAILURE;
+		}
+	}
+
+	return EXIT_SUCCESS; /* Solo se sale exitosamente si se activa el break; */
 }
